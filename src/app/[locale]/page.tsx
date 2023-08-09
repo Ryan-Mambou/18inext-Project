feb104377f06b932dffe0cd5318b4dda859d57eb
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-08-09 09:33:07 +0100

    new translations

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,8 +3,12 @@ import {useTranslations} from 'next-intl'
 export default function Home() { 
  const t = useTranslations('Index');
  return <div className='w-full h-screen bg-orange-500 text-black 
- d-flex justify-content-center items-center text-4xl'>
-<h1>{t('title')}</h1>
-
+ d-flex justify-content-center items-center'>
+<h1 className='text-4xl'>{t('title')}</h1>
+<label htmlFor="language">Select a language</label>
+<select name="language" id="language">
+<option value="english">{t("language-one")}</option>
+<option value="french">{t("language-two")}</option>
+</select>
  </div>;
 }
