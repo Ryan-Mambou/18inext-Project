fb6c7f756539afa1083b34ea1636b7c619750fa7
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-08-10 05:51:12 +0100

    reconfiguring next-intl for client components

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withNextIntl = require('next-intl/plugin')(
-  // This is the default (also the `src` folder is supported out of the box)
-  './src/i18n.ts'
-);
-
-const nextConfig = {}
-
-module.exports = withNextIntl(nextConfig);
+const nextConfig = {
+  /* config options here */
+}
+ 
+module.exports = nextConfig
 
 
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,28 @@
+'use client'
 import {useTranslations} from 'next-intl'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 export default function Home() { 
+
+  const handleSelect = (e : React.ChangeEvent<HTMLSelectElement>) => {
+    const lang : string = e.target.value;
+    if(lang === 'en'){
+      redirect('/en')
+    }
+    else {
+      redirect('fr')
+    }
+  }
+
  const t = useTranslations('Index');
  return <div className='w-full h-screen bg-orange-500 text-black 
- d-flex justify-content-center items-center'>
-<h1 className='text-4xl'>{t('title')}</h1>
-<label htmlFor="language">Select a language</label>
-<select name="language" id="language">
-<option value="english">{t("language-one")}</option>
-<option value="french">{t("language-two")}</option>
-</select>
+ flex flex-col justify-center items-center'>
+<h1 className='text-8xl'>{t('title')}</h1>
+ <label htmlFor="language" className='text-2xl mt-2'>{t('select')}</label>
+<select name="language" id="language" onChange={handleSelect} className='bg-black text-white w-32 rounded py-1 outline-none'>
+  <option value="english">{t("language-one")}</option>
+  <option value="french">{t("language-two")}</option> 
+  </select>
  </div>;
 }
